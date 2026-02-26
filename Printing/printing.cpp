3b5e60ba24b2202e74bc0994f0c56360668e8b03
Mohammed2025k <mohammed.khaled.yahia@gmail.com>
2026-02-26 03:45:32 +0200

    Enhance commints

--- Printing/printing.cpp
+++ Printing/printing.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
 #include <windows.h> // for using sleep function
-#include <chrono>
-// include is a (preprocessor directive)
-// iostream is Input/Output Stream, a library containing tools for dealing with the monitor
-// iostream includes (cout, cin, endl)
-// < > tells the compiler to find iostream in the language standard files, not in your files
+#include <chrono> // for calculate total time 
+/* 
+include is a (preprocessor directive)
+iostream is Input/Output Stream, a library containing tools for dealing with the monitor
+iostream includes (cout, cin, endl)
+< > tells the compiler to find iostream in the language standard files, not in your files
+*/
 
 using namespace std;
 using namespace chrono;
-// namespace is the "family name" for a group of tools; the standard tools of C++ are in 'std'
-// this line means if the compiler finds a tool, it automatically searches in the 'std' family
-// good for small projects, but not recommended for large ones
-
+/*
+namespace is the "family name" for a group of tools
+the standard tools of C++ are in 'std'
+this line means if the compiler finds a tool, it automatically searches in the 'std' family
+good for small projects, but not recommended for large ones
+*/
 int main()
-{
-    // cout = character output
-    // \n is just an escape sequence for endding line and move to new line
-    // endl doing the same thing and flush the buffer
+{   
+    /*
+    cout = character output
+    \n is just an escape sequence for endding line and move to new line
+    endl doing the same thing and flush the buffer
+    */
 
-    // ----------------------------------------------------------------------------------------
+    // try printing----------------------------------------------------------------------------------------
+    /*
+    cout << "I am mohammed\n";
+    cout << "Hello" << endl << "world!";
+    */
+    
 
-    // try printing
-    // cout << "I am mohammed\n";
-    // cout << "Hello" << endl << "world!";
+    // try the diffrent between endl and \n using windows.h----------------------------------------------------------------------------------------
+    /*
+    cout << "Loading...\n";
+    Sleep(3000);
 
-    // ----------------------------------------------------------------------------------------
+    cout << "Loading..." << endl;
+    Sleep(3000);
+    */
+    
 
-    // try the diffrent between endl and \n using windows.h
-    // cout << "Loading...\n";
-    // Sleep(3000);
-
-    // cout << "Loading..." << endl;
-    // Sleep(3000);
-
-    //----------------------------------------------------------------------------------------
-
-    // try the diffrent between endl and \n using chrono
+    //try the diffrent between endl and \n using chrono----------------------------------------------------------------------------------------
     auto start = high_resolution_clock::now();
     for (int i = 0; i < 1000000; i++)
     {
