911608d666344e14972f1d4c473bb8042e11b45f
Mohammed2025k <mohammed.khaled.yahia@gmail.com>
2026-02-26 08:00:55 +0200

    homework

---
+++ 01Printing/Homework.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// Homework 1
+/*
+int main()
+{
+    cout << "\n\nGuss the " << "output\n---\n";
+
+    cout << 10 + 20 - 5 << endl;
+    cout << "14/2" << "\n";
+    cout << 17 - 10 << "\n";
+    cout << "endl";
+    cout << "\nPractice makes perfect";
+
+    return 0;
+
+    cout << "\n\nBye\n\n";
+}
+*/
+
+// Homework 2
+/*
+int main()
+{
+    // Shape 1
+    cout << "*\n**\n***\n****\n*****\n\n";
+
+    // Shape 2
+    cout << "    *    \n";
+    cout << "   ***   \n";
+    cout << "  *****  \n";
+    cout << " ******* \n";
+    cout << "*********\n\n";
+
+    // Shape 3
+    cout << "  *  \n";
+    cout << " *** \n";
+    cout << "*****\n";
+    cout << " *** \n";
+    cout << "  *  \n\n";
+
+    return 0;
+}
+*/
+
+// Homework 3
