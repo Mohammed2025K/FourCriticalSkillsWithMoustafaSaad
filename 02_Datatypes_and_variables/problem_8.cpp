d7bc5c2ebdbbafe5adc6a6818fa79bee4fb4ab25
Mohammed2025k <mohammed.khaled.yahia@gmail.com>
2026-03-28 06:43:41 +0200

    problem 7

---
+++ 02_Datatypes_and_variables/problem_8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter N: ";
+    cin >> n;
+
+    cout << "The answer is: " << (n + 1) * n / 2;
+
+    return 0;
+}
